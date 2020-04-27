e793ddc719dc1534ff33bb91340d6cf60103efe1
WangLiKaiq <46625478+WangLiKaiq@users.noreply.github.com>
2020-04-27 20:17:24 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "ChessBoard.h"
+#include "test.h"
+#include <iostream>
+using namespace std;
+vector<vector<int>> testChessBoard{
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0}	
+};
+
+int main() {
+	ChessBoard myBoard( BLACK_PART, testChessBoard);
+	myBoard.run();
+	return 0;
+}
